46500781e7cb8eb88fad3560bf0b0b7c035e0c61
unknown <Cetecom@SJAPCA03LC0308.duoc.acad>
2024-08-13 11:59:41 -0400

    creacion del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'lol',
+    loadChildren: () => import('./lol/lol.module').then( m => m.LolPageModule)
+  },
 ];
 
 @NgModule({
